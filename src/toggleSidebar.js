e6a780ac71af919a29cc55bfa78ccbb947e1714c
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-29 07:10:33 +0430

    toggle sidebar

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -1,1 +1,13 @@
 import { getElement } from './utils.js';
+
+const toggleNav = getElement(".toggle-nav");
+const sidebarClose = getElement(".sidebar-close");
+const navCenter = getElement(".sidebar-overlay");
+
+toggleNav.addEventListener("click", function () {
+    navCenter.classList.add("show");
+});
+
+sidebarClose.addEventListener("click", function () {
+    navCenter.classList.remove("show");
+});
