128fd4c9b450a9fc0f3853527076a772fce46d15
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-29 19:09:29 +0430

    set up local storage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { getStorageItem, setStorageItem } from './utils.js';
-let store = [];
+let store = getStorageItem('store');
 
 const setupStore = (products) => {
     store = products.map((product) => {
@@ -12,6 +12,7 @@ const setupStore = (products) => {
         const image = img[0].thumbnails.large.url;
         return { id, colors, company, featured, price, name, image }
     });
+    setStorageItem('store', store)
 };
 const findProduct = () => { };
 export { store, setupStore, findProduct };
