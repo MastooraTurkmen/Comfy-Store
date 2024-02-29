527e2175077de13d5396bc0bb873f6cd664481a3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-29 09:58:11 +0430

    fetch products

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,10 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+    const respons = await fetch(allProductsUrl).catch(error => console.log(error))
+    if (respons) {
+        return respons;
+    }
+};
 
 export default fetchProducts;
