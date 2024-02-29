03a500c50711c0df82d1dc26b26219eb5c908bf3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-29 19:37:50 +0430

    display featured section

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -1,5 +1,30 @@
 import { formatPrice } from './utils.js';
 import { addToCart } from './cart/setupCart.js';
-const display = () => {};
+
+const display = (products, element) => {
+    element.innerHTML = products.map((product) => {
+        const { image, id, name, pirce } = product;
+        return `
+        <article class="product">
+          <div class="product-container">
+            <img src="${image}" class="product-img img" alt="${name}" />
+
+            <div class="product-icons">
+              <a href="product.html?id=${id}" class="product-icon">
+                <i class="fas fa-search"></i>
+              </a>
+              <button class="product-cart-btn product-icon" data-id="1">
+                <i class="fas fa-shopping-cart"></i>
+              </button>
+            </div>
+          </div>
+          <footer>
+            <p class="product-name">${name}</p>
+            <h4 class="product-price">$9.99</h4>
+          </footer>
+        </article>
+        `
+    }).join("")
+};
 
 export default display;
