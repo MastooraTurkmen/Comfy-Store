8b725435a88619ae6a77e22f50e5bc1b5e3bf186
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-29 08:13:08 +0430

    toggle cart

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,18 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+const cartOverlay = getElement(".cart-overlay");
+const cartClose = getElement(".cart-close");
+const toggleCart = getElement(".toggle-cart");
+
+
+toggleCart.addEventListener("click", function () {
+    cartOverlay.classList.add("show");
+});
+
+cartClose.addEventListener("click", function () {
+    cartOverlay.classList.remove("show");
+});
+
+export const openCart = () => {
+    cartOverlay.classList.add("show");
+};
