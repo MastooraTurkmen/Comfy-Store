1132e5fe975a4293c36f6ae3b83477e73dbe59e8
MastooraTurkmen <janmastoorajan@gmail.com>
2024-02-29 17:55:09 +0430

    set up store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,17 @@
 import { getStorageItem, setStorageItem } from './utils.js';
 let store = [];
-const setupStore = () => {};
-const findProduct = () => {};
+
+const setupStore = (products) => {
+    store = products.map((product) => {
+        const {
+            id,
+            field: {
+                colors, company, featured, price, name, image: img
+            },
+        } = product;
+        const image = img[0].thumbnails.large.url;
+        return { id, colors, company, featured, price, name, image }
+    });
+};
+const findProduct = () => { };
 export { store, setupStore, findProduct };
