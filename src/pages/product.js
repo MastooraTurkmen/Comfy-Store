7815846c66745afeb84fb2ef8aad1d2da269e370
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-01 19:28:18 +0430

    inner html

--- src/pages/product.js
+++ src/pages/product.js
@@ -29,7 +29,12 @@ window.addEventListener("DOMContentLoaded", async function () {
             const product = await response.json();
         } else {
             console.log(response.status, response.statusText);
-            
+            centerDOM.innerHTML = `
+            <div>
+            <h3 class="error">sorry, something went wrong</h3>
+            <a href="index.html" class="btn">back home</a>
+            </div>
+            `
         }
         
     } catch (error) {
