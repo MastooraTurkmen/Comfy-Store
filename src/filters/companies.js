679c880c44b50dc3036162fcf52487cd1f55cf11
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-01 09:01:59 +0430

    set up companies

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -1,26 +1,29 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
-import { store } from '../store.js';
 
 const setupCompanies = (store) => {
-    const companies = ['all', new Set(store.map((product) => product.company))];
-    const companiesDOM = getElement(".companies");
-    companiesDOM.innerHTML = companies.map((company) => {
-        return `<button class="company-btn">${company}</button>`
-    })
-};
+    let companies = ['all', ...new Set(store.map((product) => product.company))];
+    const companiesDOM = getElement('.companies');
 
-companiesDOM.addEventListener('click', function (e) {
-    const newCompany = e.target
-    if (newCompany.classList.contains("company-btn")) {
-        let newStore = [];
-        if (newCompany.textContent === "all") {
-            newStore = [...store];
-        } else {
-            newStore.filter((product) => product.company === e.target.textContent)
+    companiesDOM.innerHTML = companies.map((company) => {
+        return ` <button class="company-btn">${company}</button>`;
+    }).join('');
+
+    companiesDOM.addEventListener('click', function (e) {
+        const element = e.target;
+        if (element.classList.contains('company-btn')) {
+            let newStore = [];
+            if (element.textContent === 'all') {
+                newStore = [...store];
+            } else {
+                newStore = store.filter(
+                    (product) => product.company === e.target.textContent
+                );
+            }
+            
+            display(newStore, getElement('.products-container'), true);
         }
-        display(newStore, getElement(".products-container"));
-    }
-})
+    });
+};
 
 export default setupCompanies;
