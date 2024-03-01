dab8ee92081ecfa4a341746bd5729e5cf4ae20e4
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-01 09:03:52 +0430

    set up index js

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ import { setupStore, store } from './src/store.js';
 import display from './src/displayProducts.js';
 import { getElement } from './src/utils.js';
 
-
 const init = async () => {
     const products = await fetchProducts();
     if (products) {
+        // add products to the store
         setupStore(products);
-        const featured = store.filter((product) => {
-            product.featured === true
-        });
-        display(featured, getElement(".featured-center"))
+        const featured = store.filter((product) => product.featured === true);
+        display(featured, getElement('.featured-center'));
     }
-}
+};
 
-window.addEventListener("DOMContentLoaded", init)
+window.addEventListener('DOMContentLoaded', init);
