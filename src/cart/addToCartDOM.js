102c522ab7ee67a72f556f0e6e9f88e68af007b3
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-02 09:11:48 +0430

    add class and data id

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -3,6 +3,8 @@ const cartItemDOM = getElement(".cart-items")
 
 const addToCartDOM = ({ id, name, image, price, amount }) => {
     const article = document.createElement("article");
+    article.classList.add("cart-item");
+    article.setAttribute("data-id", id);
 
 };
 
