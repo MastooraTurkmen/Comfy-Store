8f26215e3e486e859fcd912ac2ac33db1a2bec6a
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-02 08:17:11 +0430

    set up cart

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,6 +10,18 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
+// const cartItemsCount = getElement(".cart-items-counts");
+const cartItems = getElement(".cart-items");
+const cartTotal = getElement(".cart-total");
+
+const cart = getElement(".cart")
+
+const init = () => {
+  console.log(cart)
+}
+
+init()
+
 export const addToCart = (id) => {
   openCart(id)
 };
