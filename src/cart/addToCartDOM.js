ed1aeef382ab7a15d44fc9daeb25ca73f5cde557
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-02 09:11:32 +0430

    set up element

--- src/cart/addToCartDOM.js
+++ src/cart/addToCartDOM.js
@@ -1,5 +1,9 @@
 import { formatPrice, getElement } from '../utils.js';
+const cartItemDOM = getElement(".cart-items")
 
-const addToCartDOM = () => {};
+const addToCartDOM = ({ id, name, image, price, amount }) => {
+    const article = document.createElement("article");
+
+};
 
 export default addToCartDOM;
