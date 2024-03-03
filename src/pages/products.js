c07e01d8515baffb6e51ee946bdbd17764d633eb
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-03 07:42:40 +0430

    products set up

--- src/pages/products.js
+++ src/pages/products.js
@@ -9,15 +9,24 @@ import setupCompanies from '../filters/companies.js';
 import setupPrice from '../filters/price.js';
 
 // specific imports
-import { store } from '../store.js';
+import { store, setupStore } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
+// import fetch products
+import fetchProducts from '../fetchProducts.js';
 
-const loading = getElement('.page-loading');
-loading.style.display = 'none';
+const init = async () => {
+    const loading = getElement('.page-loading');
+    if (store.length < 1) {
+        const products = await fetchProducts();
+        setupStore(products);
+    }
+    display(store, getElement('.products-container'));
 
-display(store, getElement(".products-container"));
+    setupSearch(store);
+    setupCompanies(store);
+    setupPrice(store);
+    loading.style.display = 'none';
+};
 
-setupSearch(store);
-setupCompanies(store);
-setupPrice(store)
+init();
