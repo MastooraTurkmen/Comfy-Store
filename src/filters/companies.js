1bfe15ad98a587231b1b0e4b11c38e34c4a587a2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-03 07:42:25 +0430

    set up companies

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -4,11 +4,11 @@ import display from '../displayProducts.js';
 const setupCompanies = (store) => {
     let companies = ['all', ...new Set(store.map((product) => product.company))];
     const companiesDOM = getElement('.companies');
-
-    companiesDOM.innerHTML = companies.map((company) => {
-        return ` <button class="company-btn">${company}</button>`;
-    }).join('');
-
+    companiesDOM.innerHTML = companies
+        .map((company) => {
+            return ` <button class="company-btn">${company}</button>`;
+        })
+        .join('');
     companiesDOM.addEventListener('click', function (e) {
         const element = e.target;
         if (element.classList.contains('company-btn')) {
@@ -20,7 +20,7 @@ const setupCompanies = (store) => {
                     (product) => product.company === e.target.textContent
                 );
             }
-            
+
             display(newStore, getElement('.products-container'), true);
         }
     });
