d67758cdc13e24f0f8c111f3e913397765bb9a82
MastooraTurkmen <janmastoorajan@gmail.com>
2024-03-03 07:42:00 +0430

    display product

--- src/displayProducts.js
+++ src/displayProducts.js
@@ -1,19 +1,19 @@
 import { formatPrice } from './utils.js';
 import { addToCart } from './cart/setupCart.js';
-
-const display = (products, element) => {
-  element.innerHTML = products.map((product) => {
-    const { image, id, name, price } = product;
-    return `
-        <article class="product">
+const display = (products, element, filters) => {
+  // display products
+  element.innerHTML = products
+    .map((product) => {
+      const { id, name, image, price } = product;
+      return ` <article class="product">
           <div class="product-container">
             <img src="${image}" class="product-img img" alt="${name}" />
-
+           
             <div class="product-icons">
               <a href="product.html?id=${id}" class="product-icon">
                 <i class="fas fa-search"></i>
               </a>
-              <button class="product-cart-btn product-icon" data-id="1">
+              <button class="product-cart-btn product-icon" data-id="${id}">
                 <i class="fas fa-shopping-cart"></i>
               </button>
             </div>
@@ -22,17 +22,18 @@ const display = (products, element) => {
             <p class="product-name">${name}</p>
             <h4 class="product-price">${formatPrice(price)}</h4>
           </footer>
-        </article>
-        `
-  }).join("");
+        </article> `;
+    })
+    .join('');
+
+  if (filters) return;
 
   element.addEventListener('click', function (e) {
-    const parent = e.target.parentElement
-    if (parent.classList.contains("product-cart-btn")) {
-      addToCart(parent.dataset.id)
+    const parent = e.target.parentElement;
+    if (parent.classList.contains('product-cart-btn')) {
+      addToCart(parent.dataset.id);
     }
   });
-
 };
 
 export default display;
